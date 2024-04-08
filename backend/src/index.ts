c351092269424e53c27373f4b996dc306a01cb5a
Supriya 007 <singhsupriya711@gmail.com>
2024-04-08 22:45:49 +0530

    db error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,10 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+}))
 
 // app.get("/api/test",async(req,res)=>{
 //     res.json({message:"hello from express endpoint!"})

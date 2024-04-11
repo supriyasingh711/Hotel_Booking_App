10cb672698f5f5f8f17102daa649ac5252505799
Supriya 007 <singhsupriya711@gmail.com>
2024-04-12 01:23:23 +0530

    validation error

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,4 +1,5 @@
 import { RegisterFormData } from "./pages/Register";
+import { SignInFormData } from "./pages/SignIn";
 
 const API_BASE_URL=import.meta.env.VITE_API_BASE_URL;
 
@@ -16,6 +17,21 @@ export const register=async (formData:RegisterFormData)=>{
         throw new Error(responseBody.message)
     }
     
+}
+export const signIn=async (FormData:SignInFormData)=>{
+    const response=await fetch(`${API_BASE_URL}\api\auth\login`,{
+        method:"POST",
+        credentials:"include",
+        headers:{
+            "Content-type":"application/json"
+        },
+        body:JSON.stringify(FormData)
+    })
+    const body=await response.json();
+    if(!response.ok){
+        throw new Error(body.message)
+    }
+    return body;
 }
 export const validateToken=async()=>{
     const response=await fetch(`${API_BASE_URL}/api/auth/validate-token`,{
---
+++ frontend/src/pages/SignIn.tsx
@@ -0,0 +1,73 @@
+import { useForm } from "react-hook-form"
+import { useMutation } from "react-query";
+import * as apiClient from '../api-client'
+
+
+
+export type SignInFormData={
+    email:string;
+    password:string;
+}
+
+
+const SignIn=()=>{
+    const {register,
+        formState:{errors},
+        handleSubmit
+    }=useForm<SignInFormData>()
+
+    const mutation=useMutation(apiClient.signIn,{
+        onSuccess:async ()=>{
+            console.log("user signed in")
+            //1.show the toast message    
+
+            //2.navigate to the home page
+
+        },onError:(error:Error)=>{
+                //show the toast
+
+        }
+            })
+
+            const onSubmit=handleSubmit((data)=>{
+                    mutation.mutate(data)
+            })
+
+    return (
+        <form className="flex flex-col gap-5 " onSubmit={onSubmit}>
+            <h2 className="text-3xl font-bold ">Sign In</h2>
+            <label className="text-gray-700 text-sm font-bold flex-1">
+                    Email<input 
+                    type="email"
+                    className="border rounded w-full py-1 px-2 font-normal"
+                    {...register("email",{required:"This field is required"})}
+                    />
+                    {errors.email && (
+                        <span className="text-red-500">{errors.email.message}</span>
+                    )}
+                </label>
+                <label className="text-gray-700 text-sm font-bold flex-1">
+                    Password<input 
+                    type="password"
+                    className="border rounded w-full py-1 px-2 font-normal"
+                    {...register("password",{
+                        required:"This field is required",
+                        minLength:{
+                            value:6,
+                            message:"Password must be 6 characters"
+                        }
+                    })}
+                    />
+                    {errors.password && (
+                        <span className="text-red-500">{errors.password.message}</span>
+                    )}
+                </label>
+                <span>
+                    <button 
+                    type="submit"
+                    className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500">Log In</button>
+                    </span>
+        </form>
+    )
+}
+export default SignIn;

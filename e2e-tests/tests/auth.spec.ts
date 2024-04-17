10046c685d255058a382fdc9db11b067c02940d0
Supriya 007 <singhsupriya711@gmail.com>
2024-04-17 22:52:11 +0530

    new email id per register test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -22,6 +22,7 @@ await expect(page.getByRole("button",{name:"Sign Out"})).toBeVisible();
 
 //test for registration
 test("should allow user to register",async({page})=>{
+  const testEmail=`test_register_${Math.floor(Math.random()*90000)+10000 }@test.com`
 
   await page.goto(UI_URL)
   await page.getByRole("link",{name:"Sign In "}).click();
@@ -29,7 +30,7 @@ test("should allow user to register",async({page})=>{
   await expect(page.getByRole("heading",{name: "Create an Account"})).toBeVisible();
   await page.locator("[name=firstName]").fill("test_firstname");
   await page.locator("[name=lastName]").fill("test_lastname");
-  await page.locator("[name=email]").fill("test@g.com");
+  await page.locator("[name=email]").fill(testEmail);
   await page.locator("[name=password]").fill("111111");
   await page.locator("[name=confirmPassword]").fill("111111");
   await page.getByRole("button",{name:"Create Account"}).click();

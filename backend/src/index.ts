7c4a44443e66b71077668e17da0dba12158b86c8
Suvam Singh <singhsupriya711@gmail.com>
2024-05-01 11:26:32 +0530

    invalid token isse fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,11 +23,11 @@ const app=express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-// app.use(cors({
-//     origin:process.env.FRONTEND_URL,
-//     credentials:true
-// }))
-app.use(cors())
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+}))
+// app.use(cors())
 
 // app.get("/api/test",async(req,res)=>{
 //     res.json({message:"hello from express endpoint!"})

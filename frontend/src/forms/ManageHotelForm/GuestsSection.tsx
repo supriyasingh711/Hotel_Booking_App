5a7e081cdbb74b885342d47a63ec7bce92f8bb35
Suvam Singh <singhsupriya711@gmail.com>
2024-05-02 23:55:51 +0530

    hotel not saving issue

--- frontend/src/forms/ManageHotelForm/GuestsSection.tsx
+++ frontend/src/forms/ManageHotelForm/GuestsSection.tsx
@@ -8,9 +8,39 @@ const GuestsSection=()=>{
 
     return (
         <div className="flex"> 
-            <label >
-                
-            </label>
+            <h2 className="text-2xl font-bold mb-3">Guests</h2>
+            <div className="grid grid-cols-2 p-6 gap-5 bg-gray-300 ">
+                <label className="text-gray-700 text-sm font-semibold">
+                    Adults
+                    <input className="border rounded w-full py-2 px-3 font-normal " type="number" min={1}
+                    {...register("adultCount",{
+                        required:"this field is required",
+                    })}
+                    />
+                    {errors.adultCount?.message && (
+                        <span className="text-red-500 text-sm font-bold">
+
+                            {errors.adultCount?.message}
+                        </span>
+                    )}
+                    </label>
+                    <label className="text-gray-700 text-sm font-semibold">
+                    Children
+                    <input className="border rounded w-full py-2 px-3 font-normal " type="number" min={0}
+                    {...register("childCount",{
+                        required:"this field is required",
+                    })}
+                    />
+                    {errors.childCount?.message && (
+                        <span className="text-red-500 text-sm font-bold">
+
+                            {errors.childCount?.message}
+                        </span>
+                    )}
+                    </label>
+                    
+                    
+            </div>
         </div>
     )
 
